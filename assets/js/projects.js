5dfddf7f69f7c07c5022fd93239ad419a91e4fae
Zed NG <zed.ng.zy@gmail.com>
2021-04-15 20:53:35 +1200

    Update

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,14 +1,18 @@
-$(function(){
-    $("#bubbles").load("bubbles.html"); 
-    $("#groundingtech").load("groundingtech.html"); 
-    $("#breathe").load("breathe.html"); 
-    $("#facade").load("facade.html"); 
-    $("#impression").load("impression.html");
-    $("#loopani").load("loopani.html"); 
-    $("#sculptex").load("sculptex.html"); 
-    $("#creature2019").load("creature2019.html"); 
-    $("#frozenmoment").load("frozenmoment.html"); 
-    $("#vitae").load("vitae.html"); 
-    $("#pod").load("pod.html"); 
-    $("#weirdwellington").load("weirdwellington.html"); 
-  });
+$(function () {
+  $("#appletoss").load("appletoss.html");
+  $("#lettinggoagain").load("lettinggoagain.html");
+  $("#bubbles").load("bubbles.html");
+  $("#groundingtech").load("groundingtech.html");
+  $("#breathe").load("breathe.html");
+  $("#facade").load("facade.html");
+  $("#impression").load("impression.html");
+  $("#serenity").load("serenity.html");
+  $("#manaaki").load("manaaki.html");
+  $("#loopani").load("loopani.html");
+  $("#sculptex").load("sculptex.html");
+  $("#creature2019").load("creature2019.html");
+  $("#frozenmoment").load("frozenmoment.html");
+  $("#vitae").load("vitae.html");
+  $("#pod").load("pod.html");
+  $("#weirdwellington").load("weirdwellington.html");
+});
